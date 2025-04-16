ee14170aea7122f6690a62e1edf01d38f6e3a588
Rosie <acquitefx@gmail.com>
2025-04-16 13:45:46 +0100

    fix: make directive expr into a field

--- grammar.js
+++ grammar.js
@@ -87,7 +87,7 @@ module.exports = grammar({
         ),
         seq($.string_literal, ":", $.type),
         seq($.character_literal, ":", $.type),
-        $.directive_expression,
+        field("directive", $.directive_expression),
       ),
 
     // Comments
